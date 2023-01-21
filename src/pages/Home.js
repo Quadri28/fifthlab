557c952428fb6b3c08d481faecb2edf215ed7fd3
User <soliuabdulkadir28@gmail.com>
2023-01-21 08:10:06 -0800

    time up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import './Home.css'
+import { FaFemale, FaMale, FaUsers } from "react-icons/fa"
 
 
 const Home = () => {
@@ -43,10 +44,34 @@ const Home = () => {
           <input type='text' className='w-100 px-4' placeholder='find a user'
             style={{ height: '4rem', borderRadius: '1em', backgroundColor: '#ededed' }} value={search} onChange={(e) => handleSearch(e)} />
         </div>
+
+        <div className="row gap-3 g-3 mt-4">
+
+        <div className='text-white col-sm-3 text-center'>
+          <button className="btn bnt-lg w-100" style={{ height: '4rem', backgroundColor: '#f935a9', 
+          color: '#fff' }}
+            onClick={() => setSearch('')}><FaUsers /></button>
+            <p style={{fontSize:'12px', color:'#ededed', marginTop:'2rem'}}>All Users</p>
+            </div>
+
+          <div className='col-sm-3 text-center text-white'>
+          <button className="btn bnt-lg w-100" style={{ height: '4rem', backgroundColor: '#30bbb5', color: '#fff' }}
+            onClick={() => setSearch('male')}><FaMale />
+            </button>
+            <p style={{fontSize:'12px', color:'#ededed', marginTop:'2rem'}}>Male Users</p>
+          </div>
+          <div className='col-sm-3 text-center text-white'>
+          <button className="btn bnt-lg w-100" style={{ height: '4rem', backgroundColor: '#7946c1', color: '#fff' }}
+            onClick={() => setSearch('female')}><FaFemale />
+            </button>
+            <p style={{fontSize:'12px', color:'#ededed', marginTop:'2rem'}}>Female Users</p>
+            </div>
+        </div>
+
       </div>
-      <div className='col-md-7 px-4 py-4 mx-auto'  style={{backgroundColor:'#ededed', borderRadius:'20px'}}>
+      <div className='col-md-7 px-4 py-4 mx-auto' style={{ backgroundColor: '#ededed', borderRadius: '20px' }}>
         <h2 className='text-dark'>All Users</h2>
-        <span style={{ fontSize: '14px', marginBottom:'1rem' }}>Filter by</span>
+        <span style={{ fontSize: '14px', marginBottom: '1rem' }}>Filter by</span>
 
         <div className='row gap-3 g-3 mb-3'>
           <div style={{ position: 'relative', marginTop: '1rem' }} className='col-sm-5'>
@@ -55,15 +80,15 @@ const Home = () => {
               style={{ height: '4rem', borderRadius: '2rem', backgroundColor: '#ededed' }} value={search} onChange={(e) => handleSearch(e)} />
           </div>
           <select type='text' style={{ height: '4rem', borderRadius: '2rem' }}
-           className='col-sm-3 p-2'>
+            className='col-sm-3 p-2'>
             <option>Country</option>
 
             {
-              countries.map((country)=>{
+              countries.map((country) => {
                 <option>{country}</option>
               })
             }
-            </select>
+          </select>
 
         </div>
         {filteredSearch.map(user => (
@@ -77,8 +102,8 @@ const Home = () => {
                 <h3 style={{ fontWeight: 700 }}>{user.name.first}</h3>
                 <p style={{ fontStyle: 'italic' }}>{user.location.street.number}, {user.location.street.name} {user.location.city} {user.location.country} </p>
                 <div className='row gap-2 g-2'>
-                  <span style={{fontSize:'13px'}} className='col-sm-6' ><i class="bi bi-envelope"></i> {user.email}</span>
-                  <span style={{fontSize:'13px'}} className='col-sm-5'> <i class='bi bi-telephone-plus'></i> {user.phone}</span>
+                  <span style={{ fontSize: '13px' }} className='col-sm-6' ><i class="bi bi-envelope"></i> {user.email}</span>
+                  <span style={{ fontSize: '13px' }} className='col-sm-5'> <i class='bi bi-telephone-plus'></i> {user.phone}</span>
                 </div>
               </div>
             </div>
